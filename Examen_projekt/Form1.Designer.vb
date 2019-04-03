cc0e4cdceea92c46b68d07493403f2de00e1bcc4
jonas wittmann <jortand122@gmail.com>
2019-04-03 14:47:46 +0200

    Layout test

--- Examen_projekt/Form1.Designer.vb
+++ Examen_projekt/Form1.Designer.vb
@@ -25,26 +25,34 @@ Partial Class Form1
         Me.New_Window_opener = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Panel_start = New System.Windows.Forms.Panel()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.KrafterToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.Panel_tést = New System.Windows.Forms.Panel()
         Me.Formel_Label = New System.Windows.Forms.Label()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EllæreToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.Panel_start.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
         Me.Panel_tést.SuspendLayout()
         Me.SuspendLayout()
         '
         'New_Window_opener
         '
-        Me.New_Window_opener.Location = New System.Drawing.Point(52, 103)
+        Me.New_Window_opener.Location = New System.Drawing.Point(24, 171)
+        Me.New_Window_opener.Margin = New System.Windows.Forms.Padding(4)
         Me.New_Window_opener.Name = "New_Window_opener"
-        Me.New_Window_opener.Size = New System.Drawing.Size(112, 58)
+        Me.New_Window_opener.Size = New System.Drawing.Size(149, 71)
         Me.New_Window_opener.TabIndex = 1
         Me.New_Window_opener.Text = "New Window"
         Me.New_Window_opener.UseVisualStyleBackColor = True
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(223, 103)
+        Me.Button1.Location = New System.Drawing.Point(24, 250)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(110, 58)
+        Me.Button1.Size = New System.Drawing.Size(147, 71)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "Same Form"
         Me.Button1.UseVisualStyleBackColor = True
@@ -54,41 +62,85 @@ Partial Class Form1
         Me.Panel_start.Controls.Add(Me.Button1)
         Me.Panel_start.Controls.Add(Me.New_Window_opener)
         Me.Panel_start.Location = New System.Drawing.Point(0, 0)
+        Me.Panel_start.Margin = New System.Windows.Forms.Padding(4)
         Me.Panel_start.Name = "Panel_start"
-        Me.Panel_start.Size = New System.Drawing.Size(404, 296)
+        Me.Panel_start.Size = New System.Drawing.Size(539, 364)
         Me.Panel_start.TabIndex = 3
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1, Me.EllæreToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(539, 28)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.KrafterToolStripMenuItem, Me.ToolStripMenuItem2})
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(76, 24)
+        Me.ToolStripMenuItem1.Text = "Mekanik"
+        '
+        'KrafterToolStripMenuItem
+        '
+        Me.KrafterToolStripMenuItem.Name = "KrafterToolStripMenuItem"
+        Me.KrafterToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.KrafterToolStripMenuItem.Text = "Krafter"
         '
         'Panel_tést
         '
         Me.Panel_tést.BackColor = System.Drawing.SystemColors.Control
         Me.Panel_tést.Controls.Add(Me.Formel_Label)
         Me.Panel_tést.Location = New System.Drawing.Point(0, 0)
+        Me.Panel_tést.Margin = New System.Windows.Forms.Padding(4)
         Me.Panel_tést.Name = "Panel_tést"
-        Me.Panel_tést.Size = New System.Drawing.Size(404, 296)
+        Me.Panel_tést.Size = New System.Drawing.Size(539, 364)
         Me.Panel_tést.TabIndex = 0
         '
         'Formel_Label
         '
         Me.Formel_Label.AutoSize = True
-        Me.Formel_Label.Location = New System.Drawing.Point(170, 126)
+        Me.Formel_Label.Location = New System.Drawing.Point(227, 155)
+        Me.Formel_Label.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Formel_Label.Name = "Formel_Label"
-        Me.Formel_Label.Size = New System.Drawing.Size(47, 13)
+        Me.Formel_Label.Size = New System.Drawing.Size(64, 17)
         Me.Formel_Label.TabIndex = 0
         Me.Formel_Label.Text = "Formel :)"
         '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(230, 26)
+        Me.ToolStripMenuItem2.Text = "Harmonisk bevægelse"
+        '
+        'EllæreToolStripMenuItem
+        '
+        Me.EllæreToolStripMenuItem.Name = "EllæreToolStripMenuItem"
+        Me.EllæreToolStripMenuItem.Size = New System.Drawing.Size(62, 24)
+        Me.EllæreToolStripMenuItem.Text = "Ellære"
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(404, 294)
+        Me.ClientSize = New System.Drawing.Size(539, 362)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.Controls.Add(Me.Panel_start)
         Me.Controls.Add(Me.Panel_tést)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.Panel_start.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.Panel_tést.ResumeLayout(False)
         Me.Panel_tést.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents New_Window_opener As Button
@@ -96,4 +148,9 @@ Partial Class Form1
     Friend WithEvents Panel_start As Panel
     Friend WithEvents Panel_tést As Panel
     Friend WithEvents Formel_Label As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents KrafterToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents EllæreToolStripMenuItem As ToolStripMenuItem
 End Class
--- Examen_projekt/Form1.vb
+++ Examen_projekt/Form1.vb
@@ -1,4 +1,7 @@
 ﻿Public Class Form1
+    Dim tal As Integer
+    Dim resultat As Double
+
     Private Sub New_Window_opener_Click(sender As Object, e As EventArgs) Handles New_Window_opener.Click
         TestForm.Show()
     End Sub
@@ -6,7 +9,16 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Panel_tést.Visible = True
         Panel_start.Visible = False
-
     End Sub
 
+    Private Sub KrafterToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KrafterToolStripMenuItem.Click
+        'TestForm.Show()
+        tal = InputBox("Indtast et tal")
+        resultat = Math.Sqrt(tal)
+        MsgBox("Kammeratroden af " & tal & " er: " & resultat)
+    End Sub
+
+    Private Sub Panel_start_Paint(sender As Object, e As PaintEventArgs) Handles Panel_start.Paint
+
+    End Sub
 End Class
